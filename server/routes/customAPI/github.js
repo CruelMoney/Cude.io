d37a840ec7e5bdb41d2070b71d84c129dd2b00f2
cruelmoney <chrdengso@gmail.com>
2017-09-04 14:02:10 +0200

    github last week working, but pretty hacky

--- server/routes/customAPI/github.js
+++ server/routes/customAPI/github.js
@@ -1,17 +1,59 @@
 'use strict';
-const getGhContribStats = require( 'github-contrib-stats')
+const requestify = require('requestify');
+const cheerio = require('cheerio');
+const moment = require('moment');
 
 exports.get = async function (req, res, next) {
     try {
-        const stats = await getGhContribStats('cruelmoney');
-        const data = 
-            {
-                todaysContributions: stats.contributionStats.todaysContributions,
-                totalContributions: stats.contributionStats.totalContributions
-            }
-        return res.send(JSON.stringify(stats))
+        const data = await getContributionStats("CruelMoney")
+        return res.send(JSON.stringify(data))        
        
       } catch (err) {
+          console.log(err)
         return next('error fetching github stats', err);
       }
 }
+
+const isoDate = date => moment(date).format('YYYY-MM-DD');
+
+// getContributionss()
+async function getContributions(username, toDate) {
+    const res = await requestify.get(`https://github.com/users/${username}/contributions?to=${isoDate(toDate)}`);
+    const $ = cheerio.load(res.getBody());
+    const fromDate = moment(toDate).subtract(1, 'd');
+    const data = [];
+    let contributions = 0;
+    let date;
+    let count;
+  
+    $('.day').each((_, e) => {
+      date = $(e).data('date');
+      count = parseInt($(e).data('count'), 10);
+      if (moment(date).unix() > fromDate.unix()) {
+        data.push({ date, count });
+        contributions += count;
+      }
+    });
+  
+    return { data, count: contributions };
+  }
+
+
+  // getContributionStats()
+async function getContributionStats(username) {
+    let fromDate = new Date() 
+    fromDate.setDate(fromDate.getDate() - 7);
+    fromDate = moment(fromDate).startOf('d');
+    let toDate = moment(new Date()).startOf('d');
+    let totalContributions = 0;
+    let contributions;
+    let data = [];
+  
+    while (toDate.unix() > fromDate.unix()) {
+      contributions = await getContributions(username, toDate);
+      totalContributions += contributions.count;
+      toDate = toDate.subtract(1, 'd').startOf('d');
+    }
+  
+    return totalContributions;
+  }
