c55e8ef0925826cadb94507fe0976e8a7e0876e5
cruelmoney <chrdengso@gmail.com>
2017-09-08 01:23:12 +0200

    new buffer function

--- server/models/CudeImage.js
+++ server/models/CudeImage.js
@@ -41,15 +41,23 @@ CudeImage.add(
 	}
 );
 
-const createThumb = async (img) => {
-    const thumbFunc = promisify(
-      img
-      .setFormat('png')
-      .resize(8,8)
-      .toBuffer.bind(img)
-    )
+function gmToBuffer (data) {
+  return new Promise((resolve, reject) => {
+    data.stream((err, stdout, stderr) => {
+      if (err) { return reject(err) }
+      const chunks = []
+      stdout.on('data', (chunk) => { chunks.push(chunk) })
+      // these are 'once' because they can and do fire multiple times for multiple errors,
+      // but this is a promise so you'll have to deal with them one at a time
+      stdout.once('end', () => { resolve(Buffer.concat(chunks)) })
+      stderr.once('data', (data) => { reject(String(data)) })
+    })
+  })
+}
 
-    const thumb = await thumbFunc();
+const createThumb = async (img) => {
+    const data = img.setFormat('png').resize(8,8);
+    const thumb = await gmToBuffer(data);
     return `data:image/png;base64,${thumb.toString('base64')}`
 }
 const deleteFile = (filename) =>{
