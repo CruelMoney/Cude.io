426fd6c67358774c5ea352774fdaae6755ca4632
Christopher Dengsø <christopher@Christophers-MacBook-Pro.local>
2017-09-16 15:11:44 +0200

    test

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Index from './Theme/index';
-import {AdminOverlay} from 'cude-cms'
-import { Route } from 'react-router-dom'
-import PropTypes from 'prop-types'
+import {AdminOverlay} from 'cude-cms';
+import { Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
   static childContextTypes = {
     staticContext: PropTypes.object
   }
-  
+
   // Static content is used by components to check if server rendering
   getChildContext() {
     return {staticContext: this.props.staticContext};
@@ -35,5 +35,3 @@ const wrappedApp = (props) => {
 }
 
 export default wrappedApp;
-
-
