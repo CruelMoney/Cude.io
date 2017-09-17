26a3cc14c092c9995dd660e5fa3a88c57c11f3aa
Christopher Dengsø <chrdengso@gmail.com>
2017-09-17 14:16:53 +0200

    importing css from cude-cms

--- src/Theme/index.js
+++ src/Theme/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Homepage from './pages/HomePage/index'
+import 'cude-cms/build/index.css'; //cms style
 import './assets/css/style.css'; //theme style
 
 export default class Index extends React.Component {
