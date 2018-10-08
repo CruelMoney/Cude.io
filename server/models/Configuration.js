9d00ce1eed9e7306b14892186c2a37fb0f763e76
Christopher Dengsø <chrdengso@gmail.com>
2018-10-08 12:16:34 +0800

    facivon

--- server/models/Configuration.js
+++ server/models/Configuration.js
@@ -1,93 +1,96 @@
-'use strict';
-const keystone = require( 'keystone');
+"use strict";
+const keystone = require("keystone");
 const Types = keystone.Field.Types;
 const storage = new keystone.Storage({
 	adapter: keystone.Storage.Adapters.FS,
 	fs: {
-		path: keystone.expandPath('../public'), // required; path where the files should be stored
-        generateFilename: (file, attempt, cb)=> {
-            if(file.extension !== "ico"){
-                cb("Favicon has to be an .ico file.")
-            }
-            cb(null, "favicon.ico")
-        },
-        whenExists: "overwrite"
+		path: keystone.expandPath("../public"), // required; path where the files should be stored
+		generateFilename: (file, attempt, cb) => {
+			file.extension = getFileExtension(file.originalname);
+
+			if (file.extension !== "ico") {
+				return cb("Favicon has to be an .ico file.");
+			}
+			return cb(null, "favicon.ico");
+		},
+		whenExists: "overwrite"
 	},
-    schema: {
-        originalname: true,
-        url: true,
-        filename: true
-    },
+	schema: {
+		originalname: true,
+		url: true,
+		filename: true
+	}
 });
 
-var SocialConfiguration = new keystone.List('SocialConfiguration', { 
-    plural: "Social",
-    label: "Social",
-    nocreate: true, //Single item
-    nodelete: true, //Single item
+function getFileExtension(filename) {
+	return filename.slice(((filename.lastIndexOf(".") - 1) >>> 0) + 2);
+}
+
+var SocialConfiguration = new keystone.List("SocialConfiguration", {
+	plural: "Social",
+	label: "Social",
+	nocreate: true, //Single item
+	nodelete: true //Single item
 });
-SocialConfiguration.add({ 
-    name: {type: String, default: "Social", noedit:true, hidden:true},
-    social:{
-        facebook:{ type: Types.Url },
-        twitter:{ type: Types.Url },
-        instagram:{ type: Types.Url },
-        snapchat:{ type: Types.Url },
-        youtube:{ type: Types.Url },
-    },
+SocialConfiguration.add({
+	name: { type: String, default: "Social", noedit: true, hidden: true },
+	social: {
+		facebook: { type: Types.Url },
+		twitter: { type: Types.Url },
+		instagram: { type: Types.Url },
+		snapchat: { type: Types.Url },
+		youtube: { type: Types.Url }
+	}
 });
-SocialConfiguration.defaultColumns = 'name|16%, social.facebook|16%, social.twitter|16%, social.instagram|16%', 'social.snapchat|16%', 'social.youtube|16%';
+(SocialConfiguration.defaultColumns =
+	"name|16%, social.facebook|16%, social.twitter|16%, social.instagram|16%"),
+	"social.snapchat|16%",
+	"social.youtube|16%";
 
 SocialConfiguration.register();
 
-
-var APIsConfiguration = new keystone.List('APIsConfiguration', { 
-    
-    label: "APIs",
-    nocreate: true, //Single item
-    nodelete: true, //Single item
+var APIsConfiguration = new keystone.List("APIsConfiguration", {
+	label: "APIs",
+	nocreate: true, //Single item
+	nodelete: true //Single item
 });
-APIsConfiguration.add({ 
-    name: {type: String, default: "APIs", noedit:true, hidden:true},
-    key: {
-        analytics: { type: Types.Key},
-        twitter: { type: Types.Key },
-        instagram: { type: Types.Key }
-    },
+APIsConfiguration.add({
+	name: { type: String, default: "APIs", noedit: true, hidden: true },
+	key: {
+		analytics: { type: Types.Key },
+		twitter: { type: Types.Key },
+		instagram: { type: Types.Key }
+	}
 });
 
 APIsConfiguration.register();
 
-
-
-
-var GeneralConfiguration = new keystone.List('GeneralConfiguration', { 
-    label: "General",
-    nocreate: true, //Single item
-    nodelete: true, //Single item
+var GeneralConfiguration = new keystone.List("GeneralConfiguration", {
+	label: "General",
+	nocreate: true, //Single item
+	nodelete: true //Single item
 });
-GeneralConfiguration.add({ 
-    name: {type: String, default: "General", noedit:true, hidden:true},
-    contact:{
-        email: {type: Types.Email, displayGravatar: true},
-        phone: {type: String},
-        address: {type: Types.Location, defaults: { country: 'Denmark' }},
-        cvr: { type: String, label: "CVR"},
-    },
-    favicon: { 
-        type: Types.File,
-        storage: storage,
-        allowedTypes: ["ico"]
-    }
+GeneralConfiguration.add({
+	name: { type: String, default: "General", noedit: true, hidden: true },
+	contact: {
+		email: { type: Types.Email, displayGravatar: true },
+		phone: { type: String },
+		address: { type: Types.Location, defaults: { country: "Denmark" } },
+		cvr: { type: String, label: "CVR" }
+	},
+	favicon: {
+		type: Types.File,
+		storage: storage,
+		allowedTypes: ["ico"]
+	}
 });
-GeneralConfiguration.defaultColumns = 'name, contact.email|20%, contact.phone|20%, contact.cvr|20%';
-
+GeneralConfiguration.defaultColumns =
+	"name, contact.email|20%, contact.phone|20%, contact.cvr|20%";
 
 GeneralConfiguration.register();
 
 exports = module.exports = {
-    GeneralConfiguration,
-    SocialConfiguration,
-    APIsConfiguration,
-}
-
+	GeneralConfiguration,
+	SocialConfiguration,
+	APIsConfiguration
+};
