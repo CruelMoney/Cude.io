58022a74441cf5796e40189c632d92b66796f2b4
Christopher Dengsø <chrdengso@gmail.com>
2018-10-08 12:41:37 +0800

    fixed button

--- src/Theme/components/Button/index.js
+++ src/Theme/components/Button/index.js
@@ -1,53 +1,48 @@
-import React from 'react';
-import styles from './index.module.css'
+import React from "react";
+import styles from "./index.module.css";
 
 class Button extends React.Component {
-  state={hover:false}
- 
-  render() {
-    let {mainColor, hoverTextColor, ...rest} = {...this.props}
-    return (
-   <div 
-    {...rest}
-   className={styles.wrapper}
-    style={{
-          ...this.props.style,
-          borderColor: this.props.mainColor,
-          backgroundColor: this.props.mainColor,
-          color: this.state.hover ? this.props.hoverTextColor : this.props.mainColor
-        }}
-   >
-     <div className={styles.hover}></div>
-     <button
-       
-        className={styles.button}
-        onMouseEnter={()=>{
-          this.setState({hover:true})
-          this.props.onMouseEnter && this.props.onMouseEnter()
-        }}
-        onMouseLeave={()=>{
-          this.setState({hover:false})
-          this.props.onMouseLeave && this.props.onMouseLeave()
-        }}
-          >
-          {this.props.href ? 
-          <a
-          href={this.props.href}
-          target={this.props.target}
-          style={null}
-          className={null}
-          >
-          {this.props.children}
-          </a>
-          :   
-          this.props.children
-          }
-        
-        </button>
-      </div>
-    );
-  }
+	state = { hover: false };
+
+	render() {
+		let { mainColor, hoverTextColor, ...rest } = { ...this.props };
+		return (
+			<div
+				{...rest}
+				className={styles.wrapper}
+				style={{
+					...this.props.style,
+					borderColor: this.props.mainColor,
+					backgroundColor: this.props.mainColor,
+					color: this.state.hover
+						? this.props.hoverTextColor
+						: this.props.mainColor
+				}}
+			>
+				<div className={styles.hover} />
+				<a
+					href={this.props.href}
+					target={this.props.target}
+					style={null}
+					className={null}
+				>
+					<button
+						className={styles.button}
+						onMouseEnter={() => {
+							this.setState({ hover: true });
+							this.props.onMouseEnter && this.props.onMouseEnter();
+						}}
+						onMouseLeave={() => {
+							this.setState({ hover: false });
+							this.props.onMouseLeave && this.props.onMouseLeave();
+						}}
+					>
+						{this.props.children}
+					</button>
+				</a>
+			</div>
+		);
+	}
 }
 
-export default Button
-
+export default Button;
